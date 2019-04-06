db9ff70c1d2a657eb1397f7c19b7f98f4f7a8b2b
Chi T. Nguyen <chinguyen@chis-air.attlocal.net>
2019-04-06 00:24:44 -0500

    Working on q1

--- HW1/crime.py
+++ HW1/crime.py
@@ -7,6 +7,9 @@ Homework 1 - Diagnostic
 import csv
 import pandas as pd
 from sodapy import Socrata
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 def get_crime_data(year, filename):
     '''
@@ -22,5 +25,45 @@ def get_crime_data(year, filename):
     for row in results:
         writer.writerow(row)
     csv_file.close()
+
+
+def analyze_crime_data():
+    '''
+    '''
+    get_crime_data(2017, "crimes_2017.csv")
+    get_crime_data(2018, "crimes_2018.csv")
+    df_2017 = pd.read_csv("crimes_2017.csv", delimiter='|')
+    df_2018 = pd.read_csv("crimes_2018.csv", delimiter='|')
+    df = pd.concat([df_2017, df_2018])
+    #crime in 2017
+    df_2017['primary_type'].value_counts(normalize=True)
+    #crime in 2018
+    df_2018['primary_type'].value_counts(normalize=True)
+    #comparing total crimes by type and year using pivot table
+    table = pd.pivot_table(df,
+                           index=['primary_type'],
+                           columns=['year','arrest'],
+                           values='id',
+                           aggfunc='count',
+                           margins=True,
+                           fill_value=0) 
+    print(table)
+    #plot the change in incidents of crime by year
+    table_for_plot = pd.pivot_table(df, 
+                                    index='primary_type',
+                                    columns='year',
+                                    values='id',
+                                    aggfunc='count',
+                                    fill_value=0) 
+    sns.set()
+    table_for_plot.plot(kind='bar')
+    plt.ylabel("Total incidents of crime")
+    plt.show()
+    #explore crime patter by ward
+
+
+
+
+
 
 
