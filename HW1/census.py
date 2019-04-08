ca3c34536d63219d7aa61dac5c61e97b1acd8039
Chi T. Nguyen <chinguyen@Chis-MacBook-Air.local>
2019-04-07 22:53:26 -0500

    Working on census

--- HW1/census.py
+++ HW1/census.py
@@ -15,11 +15,11 @@ def get_census_data():
     tables = ("B25010_001E,B19013_001E,B03002_012E,B03002_003E,"
               "B03002_004E,B03002_005E,B03002_006E,B25013_002E,"
               "B25013_007E,B25013_003E,B25013_008E")
-    col_names = []
+    col_dict = {}
     for col in tables.split(","):
         concept = censusdata.censustable('acs5',2017,col[:6])[col]['concept']
         label = censusdata.censustable('acs5',2017,col[:6])[col]['label']
-        col_names.append(label + " " + concept)
+        col_dict[col] = label + " " + concept
 
     census_api_key = '3fe9e22eeba4c4df8dec801a8308938e3de723bd'
     url = ("https://api.census.gov/data/2017/acs/acs5?"
@@ -29,6 +29,10 @@ def get_census_data():
     r = requests.get(url)
     data = r.json()
     df = pd.DataFrame(data)
+    df.columns = df.iloc[0]
+    df = df.drop(df.index[0])
+    df = df.rename(index=str, columns=col_dict)
+    return df
     
 
 
