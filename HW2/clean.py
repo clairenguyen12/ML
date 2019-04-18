9d0bc6bbaf008017b5db44a4b164ad40835cb7fe
Chi T. Nguyen <chinguyen@chis-air.attlocal.net>
2019-04-18 00:22:21 -0500

    In progress

---
+++ HW2/clean.py
@@ -0,0 +1,72 @@
+'''
+Chi Nguyen
+Machine Learning Pipeline
+'''
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+import sklearn.tree as tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score as accuracy
+import graphviz
+import os.path
+
+
+def import_csv(csv_file):
+	'''
+	'''
+    if not os.path.exists(csv_file):
+        print("File path does not exist")
+        return None
+    df = pd.read_csv(csv_file)
+    return df
+
+
+def explore_data(df):
+	'''
+	'''
+	print("Let's take a look at the first 10 lines of the dataframe!")
+	print(df.head(10))
+	print("Dataframe's shape: {}".format(df.shape))
+	print("Data types:")
+	print(df.dtypes)
+	print("Distribution of the variables in the dataframe:")
+	print(df.describe())
+
+
+def create_boxplot(df, column_name, by_variable, x_lab, y_lab, title):
+	'''
+	'''
+	df.boxplot(column=column_name, 
+			   by=by_variable).set(xlabel=x_lab, 
+			     				   ylabel=ylab)
+	plt.title(title)
+	plt.suptitle('')
+	plt.show()
+
+
+def find_outliers(df, column_name):
+	'''
+	'''
+	outliers = df[np.abs((df[column_name] - 
+	   			  df[column_name].mean()) / df[column_name].std()) > 3]
+	print("Outliers' shape:")
+	print(outliers.shape)
+
+
+def create_corr_heatmap(df):
+	'''
+	'''
+	corr = df.corr()
+	sns.heatmap(corr, 
+            	xticklabels=corr.columns, 
+            	yticklabels=corr.columns, 
+            	cmap=sns.diverging_palette(220, 20, n=7, as_cmap=True))
+
+
+
+
+
