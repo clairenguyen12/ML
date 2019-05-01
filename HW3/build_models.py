3feb3f89dd15149c4dffd19f152b31e619e7cc6f
Chi T. Nguyen <chinguyen@chis-air.attlocal.net>
2019-05-01 13:54:54 -0500

    Done testing magic loop

--- HW3/build_models.py
+++ HW3/build_models.py
@@ -33,15 +33,15 @@ from mlhelperfunctions import *
 
 RANDOM_STATE = 100
 
-def clf_loop(models_to_run, clfs, grid, test_size):
+def clf_loop(models_to_run, clfs, grid, X, y, test_size=0.3):
     """Runs the loop using models_to_run, clfs, gridm and the data
     """
-    results_df =  pd.DataFrame(columns=('model_type', 'clf', 'parameters', 
-    									'p_at_1', 'p_at_2', 'p_at_5', 
-    									'p_at_10', 'p_at_20', 'p_at_30', 'p_at_50',
-    									'recall_at_1', 'recall_at_2', 'recall_at_5',
-    									'recall_at_10', 'recall_at_20', 'recall_at_30', 'recall_at_50'	
-    									))
+    results_df =  pd.DataFrame(columns=('model_type', 'clf', 'parameters', 'baseline', 
+                                        'p_at_1', 'p_at_2', 'p_at_5', 
+                                        'p_at_10', 'p_at_20', 'p_at_30', 'p_at_50',
+                                        'recall_at_1', 'recall_at_2', 'recall_at_5',
+                                        'recall_at_10', 'recall_at_20', 'recall_at_30', 'recall_at_50',
+                                        'auc-roc'))
     for n in range(1, 2):
         # create training and valdation sets
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=RANDOM_STATE)
@@ -53,33 +53,35 @@ def clf_loop(models_to_run, clfs, grid, test_size):
                     clf.set_params(**p)
                     y_pred_probs = clf.fit(X_train, y_train).predict_proba(X_test)[:,1]
                     y_pred_probs_sorted, y_test_sorted = zip(*sorted(zip(y_pred_probs, y_test), reverse=True))
-                    results_df.loc[len(results_df)] = [models_to_run[index], clf, p,
-                                                       baseline(X_train, X_test, y_train, y_test),
-                                                       precision_at_k(y_test_sorted, y_pred_probs_sorted, 1.0),
-                                                       precision_at_k(y_test_sorted, y_pred_probs_sorted, 2.0),
-                                                       precision_at_k(y_test_sorted, y_pred_probs_sorted, 5.0),
-                                                       precision_at_k(y_test_sorted, y_pred_probs_sorted, 10.0),
-                                                       precision_at_k(y_test_sorted, y_pred_probs_sorted, 20.0),
-                                                       precision_at_k(y_test_sorted, y_pred_probs_sorted, 30.0),
-                                                       precision_at_k(y_test_sorted, y_pred_probs_sorted, 50.0),
-                                                       recall_at_k(y_test_sorted, y_pred_probs_sorted, 1.0),
-                                                       recall_at_k(y_test_sorted, y_pred_probs_sorted, 2.0),
-                                                       recall_at_k(y_test_sorted, y_pred_probs_sorted, 5.0),
-                                                       recall_at_k(y_test_sorted, y_pred_probs_sorted, 10.0),
-                                                       recall_at_k(y_test_sorted, y_pred_probs_sorted, 20.0),
-                                                       recall_at_k(y_test_sorted, y_pred_probs_sorted, 30.0),
-                                                       recall_at_k(y_test_sorted, y_pred_probs_sorted, 50.0),
-                                                       roc_auc_score(y_test, y_pred_probs)
-                                                       ]
+                    row = [models_to_run[index], clf, p,
+                           baseline(X_train, X_test, y_train, y_test),
+                           precision_at_k(y_test_sorted, y_pred_probs_sorted, 1.0),
+                           precision_at_k(y_test_sorted, y_pred_probs_sorted, 2.0),
+                           precision_at_k(y_test_sorted, y_pred_probs_sorted, 5.0),
+                           precision_at_k(y_test_sorted, y_pred_probs_sorted, 10.0),
+                           precision_at_k(y_test_sorted, y_pred_probs_sorted, 20.0),
+                           precision_at_k(y_test_sorted, y_pred_probs_sorted, 30.0),
+                           precision_at_k(y_test_sorted, y_pred_probs_sorted, 50.0),
+                           recall_at_k(y_test_sorted, y_pred_probs_sorted, 1.0),
+                           recall_at_k(y_test_sorted, y_pred_probs_sorted, 2.0),
+                           recall_at_k(y_test_sorted, y_pred_probs_sorted, 5.0),
+                           recall_at_k(y_test_sorted, y_pred_probs_sorted, 10.0),
+                           recall_at_k(y_test_sorted, y_pred_probs_sorted, 20.0),
+                           recall_at_k(y_test_sorted, y_pred_probs_sorted, 30.0),
+                           recall_at_k(y_test_sorted, y_pred_probs_sorted, 50.0),
+                           roc_auc_score(y_test, y_pred_probs)]
+                    results_df.loc[len(results_df)] = row
+                    #print(row)
                 except IndexError as e:
                     print('Error:',e)
                     continue
     return results_df
 
+
 '''
 def temporal_validation():
-	start_time_date = datetime.strptime(start_time, '%Y-%m-%d')
-	end_time_date = datetime.strptime(end_time, '%Y-%m-%d')
+    start_time_date = datetime.strptime(start_time, '%Y-%m-%d')
+    end_time_date = datetime.strptime(end_time, '%Y-%m-%d')
 
 for prediction_window in prediction_windows:
     test_end_time = end_time_date
