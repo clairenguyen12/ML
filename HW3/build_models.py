3f1a7b5184532e60a3a017114c96b8d49db0f671
Chi T. Nguyen <chinguyen@chis-air.attlocal.net>
2019-04-30 23:21:57 -0500

    Added a file

---
+++ HW3/build_models.py
@@ -0,0 +1,12 @@
+'''
+Chi Nguyen
+Improving Machine Learning Pipeline
+This file contains functions to build
+and evaluate models using different 
+classification methods
+The functions are inspired by Rayid Ghani's
+codes from the following Github repo:
+https://github.com/rayidghani/magicloops/blob/master/magicloop.py
+'''
+
+
